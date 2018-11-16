b76b300a672b76e37d6235b7bbcabded4b330879
FMocanu <florin.mocanu.studenti@isii.it>
2018-11-16 17:00:06 +0100

    test

---
+++ Server/Server/Server/frmServer.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Server {
+    partial class frmServer {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.txtQueryElaborata = new System.Windows.Forms.TextBox();
+            this.tmrTimer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnStopServer = new System.Windows.Forms.Button();
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(12, 0);
+            this.txtQuery.Multiline = true;
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(287, 213);
+            this.txtQuery.TabIndex = 0;
+            this.txtQuery.TextChanged += new System.EventHandler(this.txtQuery_TextChanged);
+            // 
+            // txtQueryElaborata
+            // 
+            this.txtQueryElaborata.Dock = System.Windows.Forms.DockStyle.Right;
+            this.txtQueryElaborata.Location = new System.Drawing.Point(316, 0);
+            this.txtQueryElaborata.Multiline = true;
+            this.txtQueryElaborata.Name = "txtQueryElaborata";
+            this.txtQueryElaborata.Size = new System.Drawing.Size(484, 450);
+            this.txtQueryElaborata.TabIndex = 1;
+            // 
+            // tmrTimer
+            // 
+            this.tmrTimer.Tick += new System.EventHandler(this.tmrTimer_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnStopServer
+            // 
+            this.btnStopServer.Location = new System.Drawing.Point(12, 319);
+            this.btnStopServer.Name = "btnStopServer";
+            this.btnStopServer.Size = new System.Drawing.Size(75, 23);
+            this.btnStopServer.TabIndex = 7;
+            this.btnStopServer.Text = "Stop Server";
+            this.btnStopServer.UseVisualStyleBackColor = true;
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(12, 269);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(75, 23);
+            this.btnStartServer.TabIndex = 8;
+            this.btnStartServer.Text = "Start Server";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            // 
+            // frmServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStartServer);
+            this.Controls.Add(this.btnStopServer);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtQueryElaborata);
+            this.Controls.Add(this.txtQuery);
+            this.Name = "frmServer";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.frmServer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.TextBox txtQueryElaborata;
+        private System.Windows.Forms.Timer tmrTimer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnStopServer;
+        private System.Windows.Forms.Button btnStartServer;
+    }
+}
+
---
+++ Server/Server/Server/frmServer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OurSqlServer.Data;
+using System.IO;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace Server {
+    public partial class frmServer : Form {
+
+        // Array che conterrà il risultato della query
+        string[,] resultquery = new string[0, 0];
+        // Stringa che conterrà la query
+        string myquery = "";
+
+        string[] parts = new string[0];
+
+        public frmServer() {
+            InitializeComponent();
+        }
+
+        private void frmServer_Load(object sender, EventArgs e) {
+
+
+            tmrTimer.Start();
+
+            // Questo parte è da modifica inserendo la stringa di query dal client
+            txtQuery.Text = "USE rwe SELECT Nome FROM Insegnanti";
+            myquery = txtQuery.Text;
+
+            //Se la stringa contenente la query non è vuota, la divide in parti
+            if (txtQuery.Text != "") {
+                //Serve per catturare il nome del database sul quale scateno la query
+                parts = myquery.Split(' ');
+            } else {
+                MessageBox.Show("Error 9, query not found!");
+            }
+
+        }
+
+        private void txtQuery_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void tmrTimer_Tick(object sender, EventArgs e) {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            try {
+                // Se la stringa contenente la query non è vuota prosegue
+                if (myquery != "") {
+
+                    /* Stringa che mi permette di conettermi al server sul quale è possibile interagire con i DB. 
+                    In altre parole è l'istanza di MySql sulla quale ho memorizzato i DB. */
+                    string connectionString = "Server=DESKTOP-BOGNLK2\\FLORINMOCANU;Database=" + parts[1] + ";" + "Trusted_Connection=True;MultipleActiveResultSets=true;";
+
+                    using (SqlConnection connection = new SqlConnection(connectionString)) {
+
+                        // Per poter eseguire il comando Sql, passo la query e la stringa di connessione
+                        SqlCommand command = new SqlCommand(myquery, connection);
+                        //command.Parameters.AddWithValue("@tPatSName", "Your-Parm-Value");
+                        // Apro la connessione
+                        connection.Open();
+
+                        // Ottengo il numero dei record affetti
+                        SqlDataReader countreader = command.ExecuteReader();
+                        int rowcount = 0;
+
+                        if (countreader.HasRows) {
+                            while (countreader.Read()) {
+                                rowcount++;
+                            }
+                        }
+                        countreader.Close();
+
+
+                        // Ottengo i valori della query eseguita, il risultato è memorizzato in un array 2D
+                        SqlDataReader reader = command.ExecuteReader();
+                        string[,] myresult = new string[rowcount, reader.FieldCount];
+                        int cont = 0;
+                        try {
+                            while (reader.Read()) {
+
+                                for (int j = 0; j < myresult.GetLength(1); j++) {
+
+                                    myresult[cont, j] = reader.GetValue(j).ToString();
+                                }
+
+                                cont++;
+
+                            }
+                        } finally {
+                            reader.Close();
+                        }
+                        resultquery = myresult;
+
+
+                    }
+                } else
+                    MessageBox.Show("Errore fatale, query non trovata!");
+            } catch {
+                if (parts[1] != "ISII" && parts[1] != "TRAMELLO")
+                    MessageBox.Show("Errore sintassi, controllare il database scelto!");
+                else
+                    MessageBox.Show("Errore sintassi SQL, controllare la query!");
+            } finally {
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+
+
+            // In mancanza di RS232 questo button mi serviva per vedere se fungeva il tutto
+            for (int i = 0; i < resultquery.GetLength(0); i++) {
+
+                for (int j = 0; j < resultquery.GetLength(1); j++) {
+
+                    txtQueryElaborata.Text += resultquery[i, j].ToString();
+                }
+
+                txtQueryElaborata.Text += "\r\n";
+            }
+        }
+
+
+    }
+}
